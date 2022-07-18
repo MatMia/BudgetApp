4e1382f6db415d14ac502d6421c8655986b135aa
MatMia <mateusz.miazowski@gmail.com>
2022-07-18 23:29:46 +0200

    categories added

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 from flask import Flask, flash, request, render_template, session, redirect, url_for
 from markupsafe import escape
-from .db import BudgetDB
+from .db import BudgetDB, CategoriesDB
 from flask_table import Table, Col
 
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+
+#main page and input form endpoints
 @app.route("/")
 def expenses_main():
-    validity_class_name = "form-control is-invalid"
-    validity_class_value = "form-control is-invalid"
-    invalid_feedback_name = ""
-    invalid_feedback_value = ""
-    input_form_buttom = "nav-link active"
-    current_budget_button = "nav-link"
-    return render_template("index.html", validity_class_name=validity_class_name, validity_class_value=validity_class_value, \
-        invalid_feedback_name=invalid_feedback_name, invalid_feedback_value=invalid_feedback_value, \
-            input_form_buttom=input_form_buttom, current_budget_button=current_budget_button)
+    return render_template("index.html", validity_class_name="form-control is-invalid", validity_class_value="form-control is-invalid", \
+        invalid_feedback_name="", invalid_feedback_value="", \
+            input_form_buttom="nav-link active", current_budget_button="nav-link", categories_button = "nav-link")
 
 @app.route('/', methods=['GET', 'POST'])
 def expense_input():
@@ -30,25 +26,25 @@ def expense_input():
     elif request.form.get("menu_current_budget") == "my_current_budget":
         return redirect(url_for('show_db_state'))
 
+    elif request.form.get("menu_categories") == "my_categories":
+        return redirect(url_for('show_db_categories'))
 
+
+#budget status endpoints
 @app.route("/db_state")
 def show_db_state():
     db_results = ShowBudgetTable.show_budget_table()
     table = db_results[0]
     value_sum = round(db_results[1],2)
-
-    input_form_buttom = "nav-link"
-    current_budget_button = "nav-link active"
-
     return render_template("budget_table.html", table=table, value_sum=value_sum, \
-        input_form_buttom=input_form_buttom, current_budget_button=current_budget_button)
+        input_form_buttom="nav-link", current_budget_button="nav-link active", categories_button = "nav-link")
 
 
 @app.route('/db_state', methods=['GET', 'POST'])
 def return_to_input():
     if request.form.get("delete_record"):
         BudgetDB.delete_record(request.form.get("delete_record"))
-        return succesfull_message('delete')
+        return succesfull_message_budget('delete')
 
     elif request.form.get("menu_input_form") == "my_input_form":
         return redirect(url_for('expenses_main'))
@@ -56,8 +52,38 @@ def return_to_input():
     elif request.form.get("menu_current_budget") == "my_current_budget":
         return redirect(url_for('show_db_state'))
 
+    elif request.form.get("menu_categories") == "my_categories":
+        return redirect(url_for('show_db_categories'))
 
-def succesfull_message(action, **kwargs):
+
+#categories endpoints
+@app.route("/categories")
+def show_db_categories():
+    table = ShowBudgetTable.show_categories_table()
+    return render_template("categories_table.html", table=table, \
+        input_form_buttom="nav-link", current_budget_button="nav-link", categories_button = "nav-link active")
+
+@app.route('/categories', methods=['GET', 'POST'])
+def categories_actions():
+    if request.form.get("delete_category_record"):
+        CategoriesDB.delete_record(request.form.get("delete_category_record"))
+        return succesfull_message_categories('delete', request.form.get("delete_category_record"))
+
+    elif request.form.get("menu_input_form") == "my_input_form":
+        return redirect(url_for('expenses_main'))
+
+    elif request.form.get("menu_current_budget") == "my_current_budget":
+        return redirect(url_for('show_db_state'))
+
+    elif request.form.get("menu_categories") == "my_categories":
+        return redirect(url_for('show_db_categories'))
+
+    elif request.form.get("add_category") == "my_category":
+        return InputForm.category_input_form()
+
+
+#flash messages handler
+def succesfull_message_budget(action, **kwargs):
     if action == 'submit':
         flash("Request submitted succesfully with \n\n name: " + kwargs["name"] + "\n\n and value: " + str(kwargs["value"] + " PLN."))
         return redirect(url_for('expenses_main'))
@@ -65,10 +91,20 @@ def succesfull_message(action, **kwargs):
         flash("Record deleted succesfully")
         return redirect(url_for('show_db_state'))
 
+def succesfull_message_categories(action, name):
+    if action == 'submit':
+        flash("Request submitted succesfully with \n\n name: " + name)
+        return redirect(url_for('show_db_categories'))
+    elif action == 'delete':
+        flash("Record deleted succesfully")
+        return redirect(url_for('show_db_categories'))
 
-def insert_to_db(expense_name, expense_value):
+
+def insert_to_budget_db(expense_name, expense_value):
     BudgetDB.insert_row(expense_name, expense_value, clear_db='N')
 
+def insert_to_categories_db(category_name):
+    CategoriesDB.insert_row(category_name, clear_db='N')
 
 def isfloat(num):
     try:
@@ -89,37 +125,17 @@ class InputFormValidation(object):
                 return True
             else:
                 return False
-        else:
-            return True
 
     def check_length(self):
-        if self.name == "expense_name":
+        if self.name == "expense_name" or self.name == "category_name":
             if len(self.key) > 2:
                 return True
             else:
                 return False
-        else:
-            return True
-
-    def validation_summary(self):
-        if InputFormValidation.check_type(self) is False:
-            invalid_feedback_value = "Value needs to be numeric."
-            summary_boolean = False
-        else:
-            invalid_feedback_value = ""
-            summary_boolean = True
-
-        if InputFormValidation.check_length(self) is False:
-            invalid_feedback_name = "Name needs to have at least 3 characters."
-            summary_boolean = False
-        else:
-            invalid_feedback_name = ""
-        
-        return (summary_boolean, invalid_feedback_name, invalid_feedback_value)
-
 
 class InputForm():
 
+    #budget input form
     def main_input_form():
         expense_name =  request.form.get("expense_name")
         expense_value = request.form.get("expense_value")
@@ -127,38 +143,70 @@ class InputForm():
         nameValidator = InputFormValidation(expense_name, "expense_name")
         valueValidator = InputFormValidation(expense_value, "expense_value")
 
-        validated_name = nameValidator.validation_summary()
-        validated_value = valueValidator.validation_summary()
-
-        invalid_feedback_name = validated_name[1]
-        invalid_feedback_value = validated_value[2]
-
-        if validated_name[0] is True and validated_value[0] is True:
-            insert_to_db(expense_name, expense_value)
-            validity_class_name = "form-control is-valid"
-            validity_class_value = "form-control is-valid"
-            return succesfull_message('submit', name=expense_name, value=expense_value)
+        # name field validation
+        if nameValidator.check_length() is False:
+            invalid_feedback_name = "Name needs to have at least 3 characters."
+            name_boolean = False
         else:
-            validity_class_name = "form-control is-invalid"
-            validity_class_value = "form-control is-invalid"
-            input_form_buttom = "nav-link active"
-            current_budget_button = "nav-link"
-            return render_template("index.html", validity_class_name=validity_class_name, validity_class_value=validity_class_value, \
+            invalid_feedback_name = ""
+            name_boolean = True
+
+        # value field validation
+        if valueValidator.check_type() is False:
+            invalid_feedback_value = "Value needs to be numeric."
+            value_boolean = False
+        else:
+            invalid_feedback_value = ""
+            value_boolean = True
+            
+        # summary validation condition    
+        if name_boolean is True and value_boolean is True:
+            insert_to_budget_db(expense_name, expense_value)
+            return succesfull_message_budget('submit', name=expense_name, value=expense_value)
+        else:
+            return render_template("index.html", validity_class_name="form-control is-invalid", validity_class_value="form-control is-invalid", \
                 invalid_feedback_name=invalid_feedback_name, invalid_feedback_value=invalid_feedback_value, \
-                    input_form_buttom=input_form_buttom, current_budget_button=current_budget_button)
+                    input_form_buttom="nav-link active", current_budget_button="nav-link", categories_button = "nav-link")
 
+    def category_input_form():
+        category_name =  request.form.get("category_name")
+        categoryValidator = InputFormValidation(category_name, "category_name")
 
-class ItemTable(Table):
+        # name field validation
+        if categoryValidator.check_length() is False:
+            invalid_feedback_category = "Category needs to have at least 3 characters."
+            category_boolean = False
+        else:
+            invalid_feedback_category = ""
+            category_boolean = True
+
+        # summary validation condition    
+        if category_boolean is True:
+            insert_to_categories_db(category_name)
+            return succesfull_message_categories('submit', category_name)
+        else:
+            flash("Category hasn't been submitted. " + invalid_feedback_category + " Please re-submit the category.")
+            return redirect(url_for('show_db_categories'))
+
+class BudgetItemTable(Table):
 
     name = Col('expense_name')
     value = Col('expense_value')
 
+class CategoriesItemTable(Table):
 
-class Item(object):
+    name = Col('categories_name')
+
+class BudgetItem(object):
 
     def __init__(self, name, value):
         self.name = name
         self.value = value
+
+class CategoriesItem(object):
+
+    def __init__(self, name):
+        self.name = name
 
 class ShowBudgetTable():
 
@@ -168,9 +216,17 @@ class ShowBudgetTable():
         value_sum = 0
         for record in db_results:
             value_sum += float(record[1])
-            table_results.append(Item(record[0],record[1]))
-        table = ItemTable(table_results)
+            table_results.append(BudgetItem(record[0],record[1]))
+        table = BudgetItemTable(table_results)
         return (table, value_sum)
+
+    def show_categories_table():
+        db_results = CategoriesDB.show_db()
+        table_results = []
+        for record in db_results:
+            table_results.append(CategoriesItem(record[0]))
+        table = CategoriesItemTable(table_results)
+        return (table)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
--- db.py
+++ db.py
@@ -4,7 +4,7 @@ class BudgetDB:
 
     def insert_row(name, value, **kwargs):
 
-        con = sqlite3.connect('example.db')
+        con = sqlite3.connect('budget.db')
         cur = con.cursor()
 
         if 'clear_db' in kwargs and kwargs['clear_db'] == 'Y':
@@ -22,7 +22,7 @@ class BudgetDB:
         con.close()
 
     def show_db():
-        con = sqlite3.connect('example.db')
+        con = sqlite3.connect('budget.db')
         cur = con.cursor()
 
         db_results = []
@@ -37,10 +37,52 @@ class BudgetDB:
 
     def delete_record(name):
         print(name)
-        con = sqlite3.connect('example.db')
+        con = sqlite3.connect('budget.db')
         cur = con.cursor()
 
         cur.execute('''DELETE FROM budget 
+        WHERE name = ?''', (name,))
+        con.commit()
+        con.close()
+
+
+class CategoriesDB:
+
+    def insert_row(name, **kwargs):
+
+        con = sqlite3.connect('categories.db')
+        cur = con.cursor()
+
+        if 'clear_db' in kwargs and kwargs['clear_db'] == 'Y':
+            cur.execute('''DROP TABLE IF EXISTS categories''')
+
+            cur.execute('''CREATE TABLE categories
+                        (name)''')
+
+        cur.execute("INSERT INTO categories VALUES (?)", (name,))
+        con.commit()
+        con.close()
+
+    def show_db():
+        con = sqlite3.connect('categories.db')
+        cur = con.cursor()
+
+        db_results = []
+
+        for row in cur.execute('''SELECT * FROM categories
+            ORDER BY name'''):
+            db_results.append(row)
+
+        con.close()
+        
+        return db_results
+
+    def delete_record(name):
+        print(name)
+        con = sqlite3.connect('categories.db')
+        cur = con.cursor()
+
+        cur.execute('''DELETE FROM categories 
         WHERE name = ?''', (name,))
         con.commit()
         con.close()
